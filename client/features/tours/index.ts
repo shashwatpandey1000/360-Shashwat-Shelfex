eacb34104d25cc88df26431e14ab76ba2a36ffb0
syd <sridhar.sing@shelfexecution.com>
2026-05-27 18:42:31 +0530

    feat(tours): export TourViewerModal from feature index

--- client/features/tours/index.ts
+++ client/features/tours/index.ts
@@ -1,3 +1,5 @@
 export { toursApi } from './api';
 export type { Tour, TourScene, TourShelf } from './api';
 export { useToursQuery, useTourByIdQuery, useActiveStoreTourQuery } from './queries';
+export { default as TourViewerModal } from './components/TourViewerModal';
+export type { TourViewerModalProps } from './components/TourViewerModal';
